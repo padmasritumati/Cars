8ca7dd31e8360a4e9d578e8e278b3d1d9ec3e20f
padmasritumati <tumatipadmasri98@gmail.com>
2021-01-26 09:19:11 +0100

    improved styling

--- src/App.js
+++ src/App.js
@@ -1,59 +1,99 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { Grid, Image, Header, Container, Message } from "semantic-ui-react";
+import { Image, Container, Button, Form, Col } from "react-bootstrap";
 
 export const imageStyle = {
   margin: "10px",
-  width: "120px",
-  height: "120px",
+  width: "220px",
+  height: "220px",
 };
 
 function App() {
   const [vinNumber, set_vinNumber] = useState("");
   const [carData, set_carData] = useState();
   const [imageData, set_imageData] = useState();
+  const [flag, set_flag] = useState(false);
+  const CAR_API_KEY = process.env.REACT_APP_CAR_API_KEY;
+  const IMAGE_API_KEY = process.env.REACT_APP_IMAGE_API_KEY;
+  let data;
 
   const search = async () => {
-    const vinNumerPathParam = encodeURIComponent(vinNumber);
+    var config = {
+      method: "get",
+      url: `https://api.overheid.io/voertuiggegevens/${vinNumber}`,
+      headers: {
+        "ovio-api-key": `${CAR_API_KEY}`,
+      },
+    };
 
-    const response = await axios.get(
-      `https://api.overheid.io/voertuiggegevens/${vinNumerPathParam}?ovio-api-key=3c98e24818644dfbe76bb5a84d5efe82547d8f0c90d19ab2f310a6d176e4c0a6`
-    );
-    set_carData(response.data);
+    await axios(config)
+      .then(function (response) {
+        data = response.data;
+        set_carData(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        set_flag(true);
+      });
 
-    const carResponce = await axios.get(
-      `https://api.unsplash.com/search/photos?query=car+${response.data.handelsbenaming}&client_id=qlsdgMYydzfmOzDhmshMLYZBQcG-7lbv2xKAEv8rPoo`
-    );
-    set_imageData(carResponce.data);
+    if (data) {
+      const carResponce = await axios.get(
+        `https://api.unsplash.com/search/photos?query=car+${data.handelsbenaming}&client_id=${IMAGE_API_KEY}`
+      );
+      set_imageData(carResponce.data);
+    }
   };
-
   return (
     <div className="App">
-      <Container>
-        <Header size="huge">Please enter you license plate number</Header>
+      <Container className="main1">
+        <h1 className="header">Please enter you license plate number</h1>
+        <Form.Row className="justify-content-md-center mt-4">
+          <Form.Group>
+            <Form.Control
+              value={vinNumber}
+              onChange={(e) => set_vinNumber(e.target.value)}
+              type="text"
+            />
+          </Form.Group>
+          <Form.Group>
+            <Button variant="dark" onClick={search}>
+              Search
+            </Button>
+          </Form.Group>
+        </Form.Row>
 
-        <p>
-          <input
-            value={vinNumber}
-            onChange={(e) => set_vinNumber(e.target.value)}
-          />
-          <button onClick={search}>Search</button>
-        </p>
-        {carData && (
+        <Container
+          className=" secondary"
+          as={Col}
+          md={{ span: 3, offset: 4.5 }}
+          fluid
+          align="center"
+        >
+          {" "}
+          {carData ? (
+            <>
+              <h2> Tread Name</h2>
+              <p>{carData.handelsbenaming}</p>
+              <h2>Data of first admission</h2>
+              <p>{carData.datum_eerste_toelating}</p>
+              <h2>Fuel type</h2>
+              <p>{carData.brandstof[0].brandstof_omschrijving}</p>
+            </>
+          ) : (
+            flag && <h2>Invalid license number</h2>
+          )}
+        </Container>
+
+        {imageData && (
           <>
-            <h2>Tread Name</h2>
-            <p>{carData.handelsbenaming}</p>
-            <h2>Data of first admission</h2>
-            <p>{carData.datum_eerste_toelating}</p>
-            <h2>Fuel type</h2>
-            <p>{carData.brandstof[0].brandstof_omschrijving}</p>
-          </>
-        )}
-        {imageData ? console.log(imageData.results, "imageData") : null}
-        {imageData ? (
-          <>
-            <Grid.Column mobile={16} computer={6} textAlign="center">
+            <Container
+              as={Col}
+              mobile={16}
+              computer={6}
+              md={{ span: 10, offset: 1 }}
+              align="center"
+            >
               <div>
                 <Image
                   src={imageData.results[0].urls.small}
@@ -77,9 +117,9 @@ function App() {
                   spaced
                 />
               </div>
-            </Grid.Column>
+            </Container>
           </>
-        ) : null}
+        )}
       </Container>
     </div>
   );
